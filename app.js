4de1a919a64bd1085297594a0b8143ef9aeb27f8
Claudio <38699823+Claudio21k@users.noreply.github.com>
2022-09-02 07:58:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+//Evento submit
+ let formulario = document.getElementById("form")
+ console.log(formulario)
+
+ formulario.addEventListener('submit', datosForm)
+
+ function datosForm(e){
+     //cancelamos por las dudas 
+     e.preventDefault();
+
+    //capturar los datos de los input
+    let datos = e.target
+    //obtenemos los datos ingresados e enviados en el evento
+  
+    console.log(datos.children[3].value)
+    console.log(datos.children[7].value)
+    console.log(datos.children[11].value)
+
+    let Nombre = datos.children[3].value;
+    let Correo = datos.children[7].value;
+    let NumeroTarjeta = datos.children[11].value;
+    let todoBien = true;
+
+    //if (!Correo.contains("@") || !Correo.contains("."))
+    if( Correo == null || Correo.length == 0 || !isNaN(Correo))
+    {
+        todoBien=false;
+        swal("Datos Incompletos ", "Correo no es valido @ o (.) ", "error")
+    }
+
+    if( NumeroTarjeta.length == 0 || NumeroTarjeta=="" || isNaN(NumeroTarjeta) ) {
+        todoBien=false;
+        swal("Datos Incompletos ", "No Ingreso los 16 digitos", "error")
+    }	
+
+    if( Nombre == null || Nombre.length == 0 || /^\s+$/.test(Nombre) || !isNaN(Nombre)) {
+        todoBien=false;
+        swal("Datos Incompletos ", "No Ingreso el nombre", "error")
+    }
+
+if (todoBien = true)
+{
+    swal("Gracias por su Compra ", "Gracias por su compra, enviamos un correo a su cuenta. Orden de Compra 125487", "success")
+}
+
+ }
+
+
+
+
+
+
+
+//Crear contenedorProductos
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+//Crear contenedorCarrito
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+// Creacion de botones
+const botonVaciar = document.getElementById('vaciar-carrito')
+const botonProcesar = document.getElementById('procesar-carrito')
+
+//Modificamos el contador del carrito
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+// Calculos Matematicos 
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const precioTotalIva = document.getElementById('precioTotalIva')
+const cantidadTotal = document.getElementById('cantidadTotal')
+const precioTransporte = document.getElementById('preciotransporte')
+const preciofinal = document.getElementById('preciofinal')
+
+let carrito = []
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+// Accion Vaciar Carrito
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+
+    swal("Vaciar Orden ", "No hay mas Articulos en la orden", "success")
+
+})
+
+//Inyectamos el HTML
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p>Posicion: ${producto.PosicionC}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+
+    `
+
+    // Agregamos el boton de Agregar
+    contenedorProductos.appendChild(div)
+    const boton = document.getElementById(`agregar${producto.id}`)
+   
+
+    boton.addEventListener('click', () => {
+        //esta funcion ejecuta el agregar el carrito con la id del producto
+        agregarAlCarrito(producto.id)
+        //
+    })
+})
+
+
+//Agaregar al carrito
+const agregarAlCarrito = (prodId) => {
+    //proceso para aumentar cantidad y que no se repita
+    //comprobar si el elemento ya existe en el carro
+    const existe = carrito.some (prod => prod.id === prodId) 
+    if (existe){ 
+        // Si el ID ya existe , actulizamos la cantidad
+        const prod = carrito.map (prod => { 
+            //creamos un nuevo arreglo e repetimos sobre cada ID y cuando
+            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { 
+        // si NO Existe , agregamos el ID al Carrito
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
+        carrito.push(item)
+    }
+    //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
+    actualizarCarrito() 
+}
+
+
+// eliminar Item del Carrito
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    //Busca el elemento y devuelve su indice.
+    const indice = carrito.indexOf(item) 
+    //Le pasamos el indice y borramos 
+    carrito.splice(indice, 1) 
+    // un elemento 
+    actualizarCarrito() 
+    console.log(carrito)
+
+    swal("Eliminar", "El Item se elimino de la Orden de Compra", "success")
+
+
+}
+
+const actualizarCarrito = () => {
+    //Vamos Agregando los productos al carrito
+    contenedorCarrito.innerHTML = "" 
+    //actualizarCarrito
+    //borrar el nodo. 
+    //recorro el array lo actualizo de nuevo 
+    //y lo rellena con la info
+    //actualizado
+    //Agrgar al modal
+    //Recorremos sobre el array de carrito.
+    //Por cada producto creamos un div con esta estructura y le hacemos un append al contenedorCarrito (el modal)
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `<p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+        contenedorCarrito.appendChild(div)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+
+    // actualizamos el Tamaño del carrito.
+    contadorCarrito.innerText = carrito.length 
+
+    //console.log(carrito)
+    precioTotal.innerText = carrito.reduce((accumulador, prod) => accumulador + prod.cantidad * prod.precio, 0)
+    precioTotalIva.innerText = carrito.reduce((accumulador, prod) =>    accumulador + (prod.cantidad * prod.precio ) *0.13   , 0)
+    
+    // Transporte es costo Fijo 2000
+    precioTransporte.innerText = 2000
+    preciofinal.innerText = 2000 + carrito.reduce((accumulador, prod) => accumulador + prod.cantidad * prod.precio, 0) + carrito.reduce((acc, prod) =>    acc + (prod.cantidad * prod.precio ) *0.13   , 0)
+    //Por cada producto en el carrito el acumulador le suma precio, Iva, Precio Final + Transporte, en base al acumulador
+    
+}
